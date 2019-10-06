d7d413684ace69a0933b49bceeda7db3fae9993c
Toya Kawakami <tojyapan925@gmail.com>
2019-10-06 23:59:20 +0900

    Filtering posts by category

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -12,34 +12,33 @@ class BlogController extends Controller
 
     public function index()
     {
-        $categories = Category::with(['posts' => function($query) {
-            $query->published();
-        }])->orderBy('title', 'asc')->get();
-
         $posts = Post::with('author')
             ->latestFirst()
             ->published()
-            ->simplepaginate($this->limit);
-        return view('blog.index', compact('posts', 'categories'));
+            ->simplePaginate($this->limit);
+            
+        return view('blog.index', compact('posts'));
     }
 
     public function show(Post $post)
     {
-        return view("blog.show")->with('post', $post);
-    }
-
-    public function category($id)
-    {
         $categories = Category::with(['posts' => function($query) {
             $query->published();
         }])->orderBy('title', 'asc')->get();
 
-        $posts = Post::with('author')
-            ->latestFirst()
-            ->published()
-            ->where('category_id', $id)
-            ->simplepaginate($this->limit);
+        return view("blog.show", compact('post'));
+    }
 
-        return view('blog.index', compact('posts', 'categories'));
+    public function category(Category $category)
+    {
+        $categoryName = $category->title;
+
+        $posts = $category->posts()
+                        ->with('author')
+                        ->latestFirst()
+                        ->published()
+                        ->simplePaginate($this->limit);
+
+        return view('blog.index', compact('posts', 'categoryName'));
     }
 }
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -6,44 +6,58 @@
         <div class="row">
             <div class="col-md-8">
 
-                @foreach ($posts as $post)
-                                    
-                    <article class="post-item">
+                @if (! $posts->count())
+                    <div class="alert alert-warning">
+                        <p>Nothing Found</p>
+                    </div>
 
-                        @if ($post->image_url)
-                            
-                            <div class="post-item-image">
-                                <a href="{{ route('blog.show', $post->id) }}">
-                                    <img src="{{ $post->image_url }}" alt="">
-                                </a>
-                            </div>
+                @else
 
-                        @endif
-
-                        <div class="post-item-body">
-                            <div class="padding-10">
-                                <h2><a href="{{ route('blog.show', $post->id) }}">{{ $post->title }}</a></h2>
-                                {!! $post->excerpt_html !!}
-                            </div>
-
-                            <div class="post-meta padding-10 clearfix">
-                                <div class="pull-left">
-                                    <ul class="post-meta-group">
-                                        <li><i class="fa fa-user"></i><a href="#">{{ $post->author->name }}</a></li>
-                                        <li><i class="fa fa-clock-o"></i><time>{{ $post->date }}</time></li>
-                                        <li><i class="fa fa-tags"></i><a href="#"> Blog</a></li>
-                                        <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
-                                    </ul>
-                                </div>
-                                <div class="pull-right">
-                                    <a href="{{ route('blog.show', $post->id) }}">Continue Reading &raquo;</a>
-                                </div>
-                            </div>
+                    @if (isset($categoryName))
+                        <div class="alert alert-info">
+                            <p>Category: <strong>{{ $categoryName }}</strong></p>
                         </div>
-                    </article>
+                    @endif
 
-                @endforeach
+                    @foreach ($posts as $post)
+                                        
+                        <article class="post-item">
 
+                            @if ($post->image_url)
+                                
+                                <div class="post-item-image">
+                                    <a href="{{ route('blog.show', $post->id) }}">
+                                        <img src="{{ $post->image_url }}" alt="">
+                                    </a>
+                                </div>
+
+                            @endif
+
+                            <div class="post-item-body">
+                                <div class="padding-10">
+                                    <h2><a href="{{ route('blog.show', $post->id) }}">{{ $post->title }}</a></h2>
+                                    {!! $post->excerpt_html !!}
+                                </div>
+
+                                <div class="post-meta padding-10 clearfix">
+                                    <div class="pull-left">
+                                        <ul class="post-meta-group">
+                                            <li><i class="fa fa-user"></i><a href="#">{{ $post->author->name }}</a></li>
+                                            <li><i class="fa fa-clock-o"></i><time>{{ $post->date }}</time></li>
+                                            <li><i class="fa fa-folder"></i><a href="{{ route('category', $post->category->slug) }}">{{ $post->category->title }}</a></li>
+                                            <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a href="{{ route('blog.show', $post->id) }}">Continue Reading &raquo;</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+
+                    @endforeach
+                
+                @endif
                 <nav>
                     {{ $posts->links() }}
                 </nav>
