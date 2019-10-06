9d89607b6f04020e70718b7f08fceeec3be733a2
Toya Kawakami <tojyapan925@gmail.com>
2019-10-06 23:23:37 +0900

    Filtering posts by category

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Category;
 
 class BlogController extends Controller
 {
@@ -11,15 +12,34 @@ class BlogController extends Controller
 
     public function index()
     {
+        $categories = Category::with(['posts' => function($query) {
+            $query->published();
+        }])->orderBy('title', 'asc')->get();
+
         $posts = Post::with('author')
             ->latestFirst()
             ->published()
             ->simplepaginate($this->limit);
-        return view('blog.index', compact('posts'));
+        return view('blog.index', compact('posts', 'categories'));
     }
 
     public function show(Post $post)
     {
         return view("blog.show")->with('post', $post);
+    }
+
+    public function category($id)
+    {
+        $categories = Category::with(['posts' => function($query) {
+            $query->published();
+        }])->orderBy('title', 'asc')->get();
+
+        $posts = Post::with('author')
+            ->latestFirst()
+            ->published()
+            ->where('category_id', $id)
+            ->simplepaginate($this->limit);
+
+        return view('blog.index', compact('posts', 'categories'));
     }
 }
