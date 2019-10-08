7d48ec261ca6828ae7063b77d08216fb406a52ac
Toya Kawakami <tojyapan925@gmail.com>
2019-10-08 18:16:07 +0900

    Add author biography and avatar

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory;
 
 class UserTableSeeder extends Seeder
 {
@@ -14,24 +15,29 @@ class UserTableSeeder extends Seeder
         DB::statement('SET FOREIGN_KEY_CHECKS=0');
         DB::table('users')->truncate();
 
+        $faker = Factory::create();
+
         DB::table('users')->insert([
             [
                 'name' => 'Jon Doe',
                 'slug' => 'jon-doe',
                 'email' => 'jondoe@test.com',
                 'password' => bcrypt('secret'),
+                'bio' => $faker->text(rand(250, 300))
             ],
             [
                 'name' => 'Ogino Chihiro',
                 'slug' => 'ogino-chihiro',
                 'email' => 'chihiro@test.com',
-                'password' => bcrypt('secret')
+                'password' => bcrypt('secret'),
+                'bio' => $faker->text(rand(250, 300))
             ],
             [
                 'name' => 'Kato Ken',
                 'slug' => 'kato-ken',
                 'email' => 'katoken@test.com',
-                'password' => bcrypt('secret')
+                'password' => bcrypt('secret'),
+                'bio' => $faker->text(rand(250, 300))
             ],
         ]);
     }
--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -15,15 +15,17 @@
 
                     @endif
 
+
+                    <?php $author = $post->author; ?>
                     <div class="post-item-body">
                         <div class="padding-10">
                             <h1>{{ $post->title }}</h1>
 
                             <div class="post-meta no-border">
                                 <ul class="post-meta-group">
-                                    <li><i class="fa fa-user"></i><a href="{{ route('author', $post->author->slug) }}">{{ $post->author->name }}</a></li>
+                                    <li><i class="fa fa-user"></i><a href="{{ route('author', $author->slug) }}">{{ $author->name }}</a></li>
                                     <li><i class="fa fa-clock-o"></i><time>{{ $post->date }}</time></li>
-                                    <li><i class="fa fa-tags"></i><a href="#"> Blog</a></li>
+                                    <li><i class="fa fa-folder"></i><a href="{{ route('category', $post->category->slug) }}">{{ $post->category->title }}</a></li>
                                     <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
                                 </ul>
                             </div>
@@ -36,20 +38,21 @@
                 <article class="post-author padding-10">
                     <div class="media">
                       <div class="media-left">
-                        <a href="#">
-                          <img alt="Author 1" src="/img/author.jpg" class="media-object">
+                        
+                        <a href="{{ route('author', $author->slug) }}">
+                            <img alt="{{ $author->name }}" width="100" height="100" src="{{ $author->gravatar() }}" class="media-object">
                         </a>
                       </div>
                       <div class="media-body">
-                        <h4 class="media-heading"><a href="{{ route('author', $post->author->slug) }}">{{ $post->author->name }}</a></h4>
+                        <h4 class="media-heading"><a href="{{ route('author', $author->slug) }}">{{ $author->name }}</a></h4>
                         <div class="post-author-count">
-                            <a href="{{ route('author', $post->author->slug) }}">
+                            <a href="{{ route('author', $author->slug) }}">
                               <i class="fa fa-clone"></i>
-                              <?php $postCount = $post->author->posts->count() ?>
+                              <?php $postCount = $author->posts()->published()->count() ?>
                               {{ $postCount }} {{ str_plural('post', $postCount) }}
                           </a>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis ad aut sunt cum, mollitia excepturi neque sint magnam minus aliquam, voluptatem, labore quis praesentium eum quae dolorum temporibus consequuntur! Non.</p>
+                        {!! $author->bio_html !!}
                       </div>
                     </div>
                 </article>
