182c6df249a9fac20b61755ed116d2d5e73ac219
Toya Kawakami <tojyapan925@gmail.com>
2019-10-09 23:11:53 +0900

    upload post image

--- resources/views/backend/blog/create.blade.php
+++ resources/views/backend/blog/create.blade.php
@@ -33,7 +33,8 @@
                         <div class="box-body ">
                           {!! Form::model($post, [
                             'method' => 'POST',
-                            'route' => 'blog.store'
+                            'route' => 'blog.store',
+                            'files' => TRUE
                           ]) !!}
 
                             <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
@@ -83,6 +84,15 @@
 
                                 @if ($errors->has('category_id'))
                                   <span class="help-block">{{ $errors->first('category_id') }}</span>
+                                @endif
+                            </div>
+
+                            <div class="form-group {{ $errors->has('image') ? 'has-error' : '' }}">
+                                {!! Form::label('image', 'Feature Image') !!}
+                                {!! Form::file('image') !!}
+
+                                @if ($errors->has('image'))
+                                  <span class="help-block">{{ $errors->first('image') }}</span>
                                 @endif
                             </div>
 
