888ede34bf8465622a6803189e40161bd11207d2
Toya Kawakami <tojyapan925@gmail.com>
2019-10-12 00:25:54 +0900

    Create role table seed data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserTableSeeder::class);
         $this->call(PostsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
     }
 }
